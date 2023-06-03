af0b6b37e6a277d9a170f48856000cf60dbba2aa
Artin Tajdini <artin@cs.washington.edu>
2023-06-02 23:23:17 -0700

    Load the trained LT to wandb

---
+++ Lottery-Ticket-Hypothesis-in-Pytorch/archs/mnist/fc1.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+class fc1(nn.Module):
+
+    def __init__(self, num_classes=10):
+        super(fc1, self).__init__()
+        self.classifier = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, num_classes),
+            nn.LogSoftmax(),
+        )
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+    
